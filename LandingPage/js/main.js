8cc4405455ffd5b33d859628291bf3007b686e47
bscode2021 <bscode@hotmail.com>
2022-08-19 20:46:31 +0200

    finished working on modal

--- LandingPage/js/main.js
+++ LandingPage/js/main.js
@@ -11,6 +11,20 @@ $(document).ready(function () {
    });
 
    $(".myCard").click(function () {
-    $(".myModal").css("display", "block");
+    let modal = $(".myModal");
+    let imgSource = $(this)[0].children[0].src;
+    modal[0].children[0].src = imgSource;
+    modal.css("display", "block");
+    $(".fadeBackground").css("display", "block");
+   });
+
+   $(".closeBtn").click(function () {
+    $(".myModal").css("display", "none");
+    $(".fadeBackground").css("display", "none");
+   });
+
+   $(".fadeBackground").click(function () {
+    $(".myModal").css("display", "none");
+    $(".fadeBackground").css("display", "none");
    })
 });
