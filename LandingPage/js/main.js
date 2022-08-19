088c5a30675842e9db20a709afcf24f4bfc6e42f
bscode2021 <bscode@hotmail.com>
2022-08-19 20:54:43 +0200

    finished dynamic cards

--- LandingPage/js/main.js
+++ LandingPage/js/main.js
@@ -1,12 +1,24 @@
 $(document).ready(function () {
+  for (let i = 1; i <= 3; i++) {
+    for (let j = 1; j <= 3; j++) {
+      $(".parent").append(
+        `<div class="myCard">
+          <img src="./img/blog-${j}.png" alt="">
+          <span>april 25, 2022</span>
+          <p>06 ways to do workout inside your home during Covid-19</p>
+        </div>`
+      );
+    }
+  }
+
    $(".myCard").fadeOut();
 
    $(document).scroll(function () {
       var y = $(this).scrollTop();
       if (y < 100) {
-         $(".myCard").fadeOut(3000);
+         $(".myCard").fadeOut(1000);
       } else {
-         $(".myCard").fadeIn(3000);
+         $(".myCard").fadeIn(1000);
       }
    });
 
