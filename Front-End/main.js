69de9feb289d4b0a52bb5432407597eca42d8415
BScode <90622569+bscode2021@users.noreply.github.com>
2022-08-23 19:24:02 +0200

    added link to jquery

--- Front-End/main.js
+++ Front-End/main.js
@@ -1,28 +1,28 @@
-window.addEventListener('load', function () {
-    let btn = document.getElementById("showText");
-    btn.addEventListener("click", showOrHideText);
+// window.addEventListener('load', function () {
+//     let btn = document.getElementById("showText");
+//     btn.addEventListener("click", showOrHideText);
 
-    function showOrHideText() {
-        let paragraphs = document.getElementsByTagName("p");
-        let myP = paragraphs[0];
+//     function showOrHideText() {
+//         let paragraphs = document.getElementsByTagName("p");
+//         let myP = paragraphs[0];
 
-        // So menuvanje na klasi
-        if (myP.classList.contains("hide")) {
-            myP.classList.remove("hide");
-            myP.classList.add("show");
+//         // So menuvanje na klasi
+//         if (myP.classList.contains("hide")) {
+//             myP.classList.remove("hide");
+//             myP.classList.add("show");
+//         } else {
+//             myP.classList.remove("show");
+//             myP.classList.add("hide");
+//         }
 
-        } else {
-            myP.classList.remove("show");
-            myP.classList.add("hide");
-        }
+//         // // So menuvanje na css
+//         // if (myP.style.display == "none") {
+//         //     myP.style.display = "block";
+//         // } else {
+//         //     myP.style.display = "none";
+//         // }
+//     }
+// }, false);
 
-        // // So menuvanje na css
-        // if (myP.style.display == "none") {
-        //     myP.style.display = "block";
-        // } else {
-        //     myP.style.display = "none";
-        // }
-    }
-}, false);
 
 
